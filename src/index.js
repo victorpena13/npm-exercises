4a4c63a69ac840f72c8cfbb463f8a3b9f37f5671
victorpena13 <victor.pena1392@gmail.com>
2019-07-19 13:43:19 -0500

    modules exercise

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-const $ = require('jquery');
+// const $ = require('jquery');
 
-const sayHello = () => 'Hello!';
-console.log(sayHello);
+// const sayHello = () => 'Hello!';
+// console.log(sayHello);
 
-$('body').css('background-color', 'purple');
+// $('body').css('background-color', 'purple');
+
+// const data = require('./say-hello.js');
+
+import data from './say-hello.js';
+
+console.log(data());
+
